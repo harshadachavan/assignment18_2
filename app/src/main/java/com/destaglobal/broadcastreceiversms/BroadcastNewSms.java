d80ebef1e5ba2908585731f352c8e868c74eb7a9
Harshada <harshada@destaglobal.com>
2017-12-24 18:42:23 +0530

    receive sms broadcast and display in toast

---
+++ app/src/main/java/com/destaglobal/broadcastreceiversms/BroadcastNewSms.java
@@ -0,0 +1,16 @@
+package com.destaglobal.broadcastreceiversms;
+
+/**
+ * Created by Harshada Chavan on 12/24/2017.
+ */
+import android.os.Bundle;
+import android.app.Activity;
+
+public class BroadcastNewSms extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
